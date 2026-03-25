0654bd8d01a634d92676aa7744c93001b7df7198
jafarzadaramin-glitch <jafarzadaramin@gmail.com>
2026-03-25 20:51:26 +0100

    fix: serve index.html without Jinja2 parsing

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_from_directory('templates', 'index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
